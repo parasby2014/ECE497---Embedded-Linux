bcc78aef7578ef0232072f484a72e7bd7a6a33b8
Ben Paras <parasby@rose-hulman.edu>
2013-10-28 14:57:01 -0400

    Updated HW9 code. I though I did it right the first time, but I didn't. So I redid everything

--- HW9/README.txt
+++ HW9/README.txt
@@ -2,12 +2,12 @@
 
 matrixLED:
 
-1.Whevever an action is taken on the browser, information is sent using socket.emit. After connect() is called, 'matrix' is sent to the bone and then the browser reads the current values on the LED matrix, converts, and then displays them. From there the browser waits for clicks and depening on where the use clicks, information is transmitted to the bone and to the browser. 
+1.Whevever an action is taken on the browser, information is sent using socket.emit. After connect() is called, 'matrix' is sent to the bone and then the browser reads the current values on the LED matrix, converts, and then displays them. From there the browser waits for clicks and depening on where the use clicks, information is transmitted to the bone using socket.emit and i2cset. The LED registers are modified accordingly to where the click occurs. 
 
-2. When an 'LED' is clicked, the LEDclick function in matrixLED.js is called and updates the LED by adding  or removing a class called 'on' which sets the color of the LED. 
+2. When an 'LED' is clicked, the LEDclick function in matrixLED.js is called and updates the LED by adding  or removing a class called 'on' which sets the color of the LED. The corresponding LED registers is also toggled. 
 
 3. the 'on' entry calss is used to color the LED's green. 
 
-4. I will control both LED's by adding two more classes for green+red and red. These are going to be define in the matrixLED.css where each class will have a specific 'background color' set. From there I will modify the LEDclick function to check the current class. If the current class is green, remove that class and add the red+green class. If the current class is red+green, remove it and add the red class. If the current class is red, remove it to turn off all LEDs. Otherwise add the green class to turn on the green LED. It's a little blunt, but it works. 
+4. I will control both LED's by adding two more classes for green+red and red. These are going to be define din the matrixLED.css where each class will have a specific 'background color' set. From there I will modify the LEDclick function to check the LED registers and set the colors accordingly in the pattern green, yellow, red, off. The program ignores the red LED registers, so I need to adjust it to read all the registers. boneServer.js also needs to be adjusted to do this as well. 
 
-5. Code is implemented and checked. Look at matrixLED.js and matrixLED.css to see changes. For some reason I need two clicks for the colors to switch and turn on. I'm not quite sure why, but either way, it works. 
+5. Code is implemented and checked. Look at matrixLED.js and matrixLED.css and boneServer.js to see changes.
--- HW9/matrixLED.js
+++ HW9/matrixLED.js
@@ -1,49 +1,74 @@
-    var socket;
+ var socket;
     var firstconnect = true,
-        i2cNum  = "0x70",
-	disp = [];
+        i2cNum = "0x70",
+        disp = [];
 
 // Create a matrix of LEDs inside the <table> tags.
 var matrixData;
 for(var j=7; j>=0; j--) {
-	matrixData += '<tr>';
-	for(var i=0; i<8; i++) {
-	    matrixData += '<td><div class="LED" id="id'+i+'_'+j+
-		'" onclick="LEDclick('+i+','+j+')">'+
-		i+','+j+'</div></td>';
-	    }
-	matrixData += '</tr>';
+        matrixData += '<tr>';
+        for(var i=0; i<8; i++) {
+         matrixData += '<td><div class="LED" id="id'+i+'_'+j+
+                '" onclick="LEDclick('+i+','+j+')">'+
+                i+','+j+'</div></td>';
+         }
+        matrixData += '</tr>';
 }
 $('#matrixLED').append(matrixData);
 
 // Send one column when LED is clicked.
-function LEDclick(i, j) {
+function LEDclick(i, j) 
+{
     //alert(i+","+j+" clicked");
-    disp[i] ^= 0x1<<j;
-    socket.emit('i2cset', {i2cNum: i2cNum, i: i, 
-			     disp: '0x'+disp[i].toString(16)});
-	socket.emit('i2c', i2cNum);
-    // Toggle bit on display
-    if(disp[i]>>j&0x1 === 1) {
-      	if($('#id' + i + '_' + j).hasClass('onG'))
+  	// multiply i by 2 to get all the 16 bits
+	i = (i<<1);
+	
+	//check current color and run the pattern green, both, red, off
+
+	//if green, toggle red on to get both
+	if ((((disp[i] >> j) & 0x1) === 1)&&(((disp[i+1] >> j) & 0x1) === 0)) 
 	{
-		$('#id' + i + '_' + j).removeClass('onG').addClass('onGR');
-	}
-	else if($('#id' + i + '_' + j).hasClass('onGR'))
+        	disp[i+1] ^= 0x1<<j;
+    	} // if both, toggle green off to get red    
+	else if ((((disp[i] >> j) & 0x1) === 1)&&(((disp[i+1] >> j) & 0x1) === 1))
 	{
-		$('#id' + i + '_' + j).removeClass('onGR').addClass('onR');
-	}
-	else if($('#id' + i + '_' + j).hasClass('onR'))
+		disp[i] ^= 0x1<<j;
+	} // if red, then toggle red off to keep all off 
+	else if ((((disp[i] >> j) & 0x1) === 0)&&(((disp[i+1] >> j) & 0x1) === 1))
 	{
-		$('#id' + i + '_' + j).removeClass('onR');
-	}
+		disp[i+1] ^= 0x1<<j;
+	} // otherwise, toggle green on
 	else
 	{
-		$('#id' + i + '_' + j).addClass('onG');
+		disp[i] ^= 0x1<<j;
 	}
 
-   }
-		 
+	// send to bone
+	socket.emit('i2cset', {i2cNum: i2cNum, i: i,
+                         disp: '0x'+disp[i].toString(16)});
+
+        socket.emit('i2cset', {i2cNum: i2cNum, i: i+1,
+                         disp: '0x'+disp[i+1].toString(16)});
+   	
+	
+	// Toggle bit on display, divide i by 2 to undo the multiplcation
+	// if green, set browser to green
+	if ((((disp[i] >> j) & 0x1) === 1)&&(((disp[i+1] >> j) & 0x1) === 0)) 
+	{
+        	$('#id' + (i>>1) + '_' + j).addClass('onG');
+    	} // if both, set browser to both
+	else if ((((disp[i] >> j) & 0x1) === 1)&&(((disp[i+1] >> j) & 0x1) === 1))
+	{
+		$('#id' + (i>>1)  + '_' + j).removeClass('onG').addClass('onGR');
+	} //if both set browser to red
+	else if ((((disp[i] >> j) & 0x1) === 0)&&(((disp[i+1] >> j) & 0x1) === 1))
+	{
+		$('#id' + (i>>1)  + '_' + j).removeClass('onGR').addClass('onR');
+	} // othwersie turn it off
+	else
+	{
+		$('#id' + (i>>1)  + '_' + j).removeClass('onR');
+	}       
 
 }
 
@@ -66,8 +91,8 @@ function LEDclick(i, j) {
         socket.on('reconnect_failed', function()
             { message("Reconnect Failed"); });
 
-        socket.on('matrix',  matrix);
-        // Read display for initial image.  Store in disp[]
+        socket.on('matrix', matrix);
+        // Read display for initial image. Store in disp[]
         socket.emit("matrix", i2cNum);
 
         firstconnect = false;
@@ -86,36 +111,49 @@ function LEDclick(i, j) {
     function matrix(data) {
         var i, j;
         disp = [];
-        //        status_update("i2c: " + data);
+        // status_update("i2c: " + data);
         // Make data an array, each entry is a pair of digits
         data = data.split(" ");
-        //        status_update("data: " + data);
+        status_update("data: " + data);
         // Every other pair of digits are Green. The others are red.
         // Ignore the red.
         // Convert from hex.
         for (i = 0; i < data.length; i += 1) {
             disp[i] = parseInt(data[i], 16);
         }
-        //        status_update("disp: " + disp);
+        // status_update("disp: " + disp);
         // i cycles through each column
-        for (i = 0; i < disp.length; i++) {
+        for (i = 0; i < (disp.length); i+=2) 
+	{
             // j cycles through each bit
-            for (j = 0; j < 8; j++) {
-                if (((disp[i] >> j) & 0x1) === 1) {
-                    $('#id' + i + '_' + j).addClass('onG');
-                } else {
-                    $('#id' + i + '_' + j).removeClass('onG');
+		for (j = 0; j < 8; j++) 
+		{
+		//clear the colors first
+		$('#id' + i + '_' + j).removeClass('onG onGR onR');
+		
+		//check if green LED is on or if both are on or red LED is on and set it on the browser
+                if ((((disp[i] >> j) & 0x1) === 1)&&(((disp[i+1] >> j) & 0x1) === 0)) 
+		{
+                	$('#id' + (i>>1) + '_' + j).addClass('onG');
+                } 
+		else if ((((disp[i] >> j) & 0x1) === 1)&&(((disp[i+1] >> j) & 0x1) === 1))
+		{
+			$('#id' + (i>>1) + '_' + j).addClass('onGR');
+		}
+		else if ((((disp[i] >> j) & 0x1) === 0)&&(((disp[i+1] >> j) & 0x1) === 1))
+		{
+                    	$('#id' + (i>>1) + '_' + j).addClass('onR');
                 }
             }
         }
     }
 
     function status_update(txt){
-	$('#status').html(txt);
+        $('#status').html(txt);
     }
 
     function updateFromLED(){
-      socket.emit("matrix", i2cNum);    
+      socket.emit("matrix", i2cNum);
     }
 
 connect();
